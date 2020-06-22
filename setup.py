293909dd7551f103822d036e43774ae929cf1f2b
Dmytro Dovbii <dmanddov@gmail.com>
2020-06-22 14:50:28 +0300

    Add python3 support
    * Added python3 support
    * Requirements updated to newest versions
    * Fixed imports
    * Bump up package version

--- setup.py
+++ setup.py
@@ -28,18 +28,16 @@ with open("test_requirements.txt") as f_tests:
     required_for_tests = f_tests.read().splitlines()
 
 setup(
-    name="cloudshell-template",
+    name="cloudshell-networking-cisco-nxos",
     url="http://www.qualisystems.com/",
     author="QualiSystems",
     author_email="info@qualisystems.com",
     packages=find_packages(),
     install_requires=required,
     tests_require=required_for_tests,
-    python_requires=(
-        ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <4"
-    ),
+    python_requires="~=3.7",
     version=version_from_file,
     package_data={"": ["*.txt"]},
-    description="<your package description>",
+    description="QualiSystems networking cisco NxOS specific Package",
     include_package_data=True,
 )

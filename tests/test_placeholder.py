4db90675afdef02714ac9a1bc8e3277ef66d0ea0
Dmytro Dovbii <dmanddov@gmail.com>
2020-06-22 14:50:28 +0300

    Add python3 support
    * Added python3 support
    * Requirements updated to newest versions
    * Fixed imports
    * Bump up package version

---
+++ tests/test_placeholder.py
@@ -0,0 +1,11 @@
+import unittest
+
+from cloudshell.networking.cisco.nxos.command_templates import nxos_add_remove_vlan  # noqa: F401
+
+
+class TestEmpty(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_smth(self):
+        self.assertTrue(True)
